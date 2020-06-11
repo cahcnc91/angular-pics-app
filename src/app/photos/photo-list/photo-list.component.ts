faefa0faa0cfdd9c4fcfc5088fadacea5f9e1876
cahcnc91 <camilanierocosta@hotmail.com>
2020-06-11 19:29:14 -0400

    feat: pics app displaying pictures done

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Photo } from '../photo/photo';
+import { ActivatedRoute } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Photo[] = [];
+  filter: string = '';
+  hasMore : boolean = true;
+  currentPage: number = 1;
+  userName : string = '';
+  constructor(
+    private activedRoute: ActivatedRoute, private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.userName = this.activedRoute.snapshot.params.userName;
+    this.photos = this.activedRoute.snapshot.data.photos;
+  }
+
+  load(){
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.filter = '';
+      this.photos = this.photos.concat(photos)
+      if(!photos.length) this.hasMore = false;
+    })
+  }
+
+}
